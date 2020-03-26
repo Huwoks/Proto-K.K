74408c892c2403fe588364af00eb727d5c943c94
Jose Zavala <170300126@ucaribe.edu.mx>
2020-03-25 22:35:28 -0500

    ,

---
+++ frontend/src/components/Cart.vue
@@ -0,0 +1,20 @@
+<template>
+    <h2>{{ pito }}</h2>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            pito: 'Pito'
+        }
+    },
+    methods: {
+        
+    },
+}
+</script>
+
+<style lang="css" scoped>
+
+</style>
