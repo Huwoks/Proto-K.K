315e015f7709849d325455e0ab973f41b0e320cc
Jose Zavala <170300126@ucaribe.edu.mx>
2020-04-01 21:23:10 -0500

    Cambios aceptados, ambiente de desarrollo arreglado

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,37 +2,14 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Cart from '@/components/Cart'
-<<<<<<< HEAD
-import Button from '@/components/example/Button.vue'
-=======
 import Navbar from '@/components/Navbar'
 import { NavbarPlugin } from 'bootstrap-vue'
->>>>>>> 7d55e9da7581af3b025b6871aa9c1fc35dbb0f17
 
 Vue.use(Router)
 
 export default new Router({
-<<<<<<< HEAD
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/cart',
-      name: 'Cart',
-      component: Cart
-    },
-    {
-      path: '/button',
-      name: 'Button',
-      component: Button
-    }
-  ]
-})
-=======
-    routes: [{
+    routes: [
+        {
             path: '/',
             name: 'HelloWorld',
             component: HelloWorld
@@ -49,4 +26,3 @@ export default new Router({
         }
     ]
 })
->>>>>>> 7d55e9da7581af3b025b6871aa9c1fc35dbb0f17
