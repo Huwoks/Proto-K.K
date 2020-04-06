ec6d9049ff0aea0ac5def80b62e8164b89507f06
Hugo Ramón <hugo_ramon99@outlook.es>
2020-04-06 18:35:56 -0500

    Bug FIxes - Navbar

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,10 +1,11 @@
 <template>
-  <div class="align-content-start">
+  <div >
     <nav class="main-navigation">
       <b-navbar
         toggleable="lg"
         type="dark"
-        variant="danger"
+        variant="dark"
+        
         fixed="top"
         position="fixed"
       >
@@ -30,10 +31,10 @@
                 <ul class="sub-menu">
                   <li><a href="#">Carbón</a></li>
                   <li><a href="#">Metal</a></li>
-                </ul>
+                </ul> 
               </li>
               <li class="menu-item-has-children">
-                <a href="#">Capcitores</a>
+                <a href="#"> Capacitores    </a>
                 <ul class="sub-menu">
                   <li><a href="#">Cerámicos</a></li>
                   <li><a href="#">Electrolíticos</a></li>
@@ -70,13 +71,122 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-* {
-  background:linear-gradient(180deg, #0288D1,  #0097A7);
+.navbar.navbar-dark.bg-dark{
+    background: linear-gradient(180deg, #0288D1,  #0097A7);
+
+ }
+*{ 
+  box-sizing:border-box;
+  font-family:"Dosis", sans-serif; 
+}
+
+.main-navigation{
+  border-top:1px solid #fff;
+  border-bottom:1px solid #fff;
+  margin:2em;
+}
+
+ul{
+  margin:0; //reset
+  padding:0;
+}
+
+.menu{
+  display:flex;  
+  flex-direction:column;
+  
+  li{
+    display:block;
+    list-style-type:none;
+    
+  }
+  
+  li a{
+    display:flex;
+    text-align:center;
+    text-decoration:none;
+    color:#fff;
+    padding:1rem;
+  }
+  
+  li a:hover{
+    
+    color:black;
+  }
+} 
+
+@media (min-width: 800px) {
+    .menu{
+      flex-direction:row;
+      justify-content:space-between;
+    }
+}
+
+
+
+nav ul ul {
+    display: none;
+}
+
+@media (min-width: 800px) {
+  nav ul ul {
+    display: none;
+    position:absolute;
+  }
+}
+
+nav ul li:hover > ul {
+  display:block;
+  color:#fff;
+}
+
+@media (min-width: 800px) {
+  nav ul li:hover > ul {
+    background: linear-gradient(180deg, #0288D1,  #0097A7);
+  }
+  
+  nav ul li:hover > ul a{
+    color:#fff;
+  }
+  
+  .sub-menu{
+    li{
+      display:flex;
+      
+    }
+    
+    li:hover, li a:hover{
+      color:black;
+    }
+  }
+  
+  nav ul ul ul{
+    margin:0 0 0 100%;
+    box-shadow: 0 0 2px rgba(0, 0, 0, 0.6);
+  }
+  
+  
+  
+}
+.footer{
+    margin-top: 100%;
+    right: 0%;
+    left: 0%;
+    position:absolute;
+  }
+/** {
+  
   box-sizing: border-box;
   font-family: sans-serif;
 }
+.navbar.navbar-dark.bg-dark{
+    background: linear-gradient(180deg, #0288D1,  #0097A7);
+
+ }
+
 
 .main-navigation {
+  
   border-top: 1px solid #ddd;
   border-bottom: 1px solid #ddd;
   margin: 2em;
@@ -107,7 +217,8 @@ ul {
   }
 
   li a:hover {
-    background: #fff;
+    display:flex;
+    
     color: black;
   }
 }
@@ -115,18 +226,20 @@ ul {
 @media (min-width: 800px) {
   .menu {
     flex-direction: row;
+    
     justify-content: space-between;
   }
 }
 
 .menu-item-has-children > a:after {
   font-family: sans-serif;
-
+  
   padding: 1px;
 }
 
 nav ul ul {
   display: none;
+  
 }
 
 @media (min-width: 700px) {
@@ -138,49 +251,67 @@ nav ul ul {
 
 nav ul li:hover > ul {
   display: block;
-  color: #fff;
+  
   padding-left: 2rem;
 }
 
 @media (min-width: 800px) {
+  
   nav ul li:hover > ul {
     padding-left: 0;
-    background: #777;
+    color:black;
+    
+  background: linear-gradient(180deg, #0288D1,  #0097A7);
+    //background-color: #ddd;
+   // 
+;
   }
 
   nav ul li:hover > ul a {
     color: #fff;
+    background-color: #fff;
+  background: linear-gradient(180deg, #0288D1,  #0097A7); 
   }
 
   .sub-menu {
+   
+
     li {
+      color:black;
       display: flex;
+      
       padding-right: 2em;
     }
 
     li:hover,
     li a:hover {
-      background: #ddd;
-      color: #777;
+      color: black;
+      
+      
     }
   }
 
   nav ul ul ul {
+    color:black;
     margin: 0 0 0 100%;
     box-shadow: 0 0 2px rgba(0, 0, 0, 0.6);
   }
 
   .menu-item-has-children .menu-item-has-children > a:after {
+    
+   
     font-family: sans-serif;
     content: ">";
+    
     padding: 1px;
     transform: rotate(0deg);
   }
+  
   .footer{
     margin-top: 100%;
     right: 0%;
     left: 0%;
     position:absolute;
   }
-}
+}*/
 </style>
