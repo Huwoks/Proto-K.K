2606ec4c4f84b93870d045b23c2f948e1f1729b7
Hugo Ramón <hugo_ramon99@outlook.es>
2020-04-27 16:00:37 -0500

    CategoryPage View

---
+++ frontend/src/components/exptable.vue
@@ -0,0 +1,21 @@
+<template>
+<div>     
+        <Table class="Table" position="absolute" fixed="middle"/>
+        
+    
+        <Navbar class="Navbar"/>
+    </div>
+</template>
+
+<script>
+import Navbar from "./Navbar";
+import Table from "./Table";
+export default {
+  components: {
+    Navbar,
+    Table
+
+  }
+};
+
+</script>
