afc449808ccbd78466549e67895a8b7acffe1144
Hugo Ramón <hugo_ramon99@outlook.es>
2020-05-12 12:39:53 -0500

    CategoryPage Component

--- frontend/src/components/CategoryPage.vue
+++ frontend/src/components/CategoryPage.vue
@@ -9,16 +9,16 @@
             <div class="row">
                 <div class="col-12 col-md-6 col-lg-4">
                     <div class="card">
-                        <img class="card-img-top" src="https://dummyimage.com/600x400/55595c/fff" alt="Card image cap">
+                        <img class="card-img-top" src="https://scontent-qro1-1.xx.fbcdn.net/v/t1.0-9/89656671_113705190242843_6149278486419734528_n.png?_nc_cat=104&_nc_sid=0debeb&_nc_ohc=kLRW01WSFMIAX9so2HX&_nc_ht=scontent-qro1-1.xx&oh=66b41da6cd2bab80a968143631dcc883&oe=5EDDF114" alt="Card image cap">
                         <div class="card-body">
-                            <h4 class="card-title"><a href="product.html" title="View Product">Product title</a></h4>
-                            <p class="card-text">Description.</p>
+                            <h4 class="card-title"><a href="product.html" title="View Product">Resistencia 220 kΩ, 3W</a></h4>
+                            <p class="card-text"> Resistencia 220 kΩ, 3W </p>
                             <div class="row">
                                 <div class="col">
-                                    <p class="btn btn-danger btn-block">$0.00 MXN</p>
+                                    <p class="btn btn-danger btn-block">$3.00 MXN</p>
                                 </div>
                                 <div class="col">
-                                    <a href="#" class="btn btn-success btn-block">Add to cart</a>
+                                    <a href="#" class="btn btn-cart btn-block">Add to cart</a>
                                 </div>
                             </div>
                         </div>
@@ -26,16 +26,36 @@
                 </div>
                 <div class="col-12 col-md-6 col-lg-4">
                     <div class="card">
-                        <img class="card-img-top" src="https://dummyimage.com/600x400/55595c/fff" alt="Card image cap">
+                        <img class="card-img-top" src="https://scontent-qro1-1.xx.fbcdn.net/v/t1.0-9/89728105_113711923575503_3625148699304263680_n.png?_nc_cat=103&_nc_sid=0debeb&_nc_ohc=WRoK8kHnrQIAX-Igw7-&_nc_ht=scontent-qro1-1.xx&oh=65cc00cd322ad96d23ff2c3fa4d73e7c&oe=5EDE9A58" alt="Card image cap">
                         <div class="card-body">
-                            <h4 class="card-title"><a href="product.html" title="View Product">Product title</a></h4>
-                            <p class="card-text">Description.</p>
+                            <h4 class="card-title"><a href="product.html" title="View Product">Resistencia 100Ω, 1/4W</a></h4>
+                            <p class="card-text">Resistencia 100Ω, 1/4W</p>
+                           
                             <div class="row">
                                 <div class="col">
-                                    <p class="btn btn-danger btn-block">$0.00 MXN</p>
+                                    <p class="btn btn-danger btn-block">$3.00 MXN</p>  
+                                </div>
+
+                                <div class="col">
+                                    <a href="#" class="btn btn-cart btn-block">Add to cart</a>
+                                </div>
+          
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="card">
+                        <img class="card-img-top" src="https://scontent-qro1-1.xx.fbcdn.net/v/t1.0-9/89656554_113727013573994_8091811260036808704_n.png?_nc_cat=103&_nc_sid=0debeb&_nc_ohc=OAqiAJLKK98AX9Ajg9t&_nc_ht=scontent-qro1-1.xx&oh=248cd764197cbd6d88f8c2ef35312367&oe=5EE158C9" alt="Card image cap">
+                        <div class="card-body">
+                            <h4 class="card-title"><a href="product.html" title="View Product">Resistencia de 22 KΩ, 5 W</a></h4>
+                            <p class="card-text">Resistencia de 22 KΩ, 5 W</p>
+                            <div class="row">
+                                <div class="col">
+                                    <p class="btn btn-danger btn-block">$6.00 MXN</p>
                                 </div>
                                 <div class="col">
-                                    <a href="#" class="btn btn-success btn-block">Add to cart</a>
+                                    <a href="#" class="btn btn-cart btn-block">Add to cart</a>
                                 </div>
                             </div>
                         </div>
@@ -43,16 +63,16 @@
                 </div>
                 <div class="col-12 col-md-6 col-lg-4">
                     <div class="card">
-                        <img class="card-img-top" src="https://dummyimage.com/600x400/55595c/fff" alt="Card image cap">
+                        <img class="card-img-top" src="https://scontent-qro1-1.xx.fbcdn.net/v/t1.0-9/89840450_113727020240660_2242552491398922240_n.png?_nc_cat=103&_nc_sid=0debeb&_nc_ohc=mzfEqsow9SsAX-L0CvY&_nc_ht=scontent-qro1-1.xx&oh=cfcffd400f8aec735ed6845d0dc55ce1&oe=5EDF4EEC" alt="Card image cap">
                         <div class="card-body">
-                            <h4 class="card-title"><a href="product.html" title="View Product">Product title</a></h4>
-                            <p class="card-text">Description.</p>
+                            <h4 class="card-title"><a href="product.html" title="View Product">Resistencia de 10 Ω, 2W</a></h4>
+                            <p class="card-text">Resistencia de 10 Ω, 2W</p>
                             <div class="row">
                                 <div class="col">
-                                    <p class="btn btn-danger btn-block">$0.00 MXN</p>
+                                    <p class="btn btn-danger btn-block">$6.00 MXN</p>
                                 </div>
                                 <div class="col">
-                                    <a href="#" class="btn btn-success btn-block">Add to cart</a>
+                                    <a href="#" class="btn btn-cart btn-block">Add to cart</a>
                                 </div>
                             </div>
                         </div>
@@ -60,16 +80,16 @@
                 </div>
                 <div class="col-12 col-md-6 col-lg-4">
                     <div class="card">
-                        <img class="card-img-top" src="https://dummyimage.com/600x400/55595c/fff" alt="Card image cap">
+                        <img class="card-img-top" src="https://scontent-qro1-1.xx.fbcdn.net/v/t1.0-9/89987603_113727650240597_9004930794749165568_n.png?_nc_cat=102&_nc_sid=0debeb&_nc_ohc=kx58afJUFnoAX_k-khL&_nc_ht=scontent-qro1-1.xx&oh=e79dcad5cf6b4c8446d00904d9950f34&oe=5EDE89E1" alt="Card image cap">
                         <div class="card-body">
-                            <h4 class="card-title"><a href="product.html" title="View Product">Product title</a></h4>
-                            <p class="card-text">Descripion.</p>
+                            <h4 class="card-title"><a href="product.html" title="View Product">Resistencia 240 Ω, 1/4 W</a></h4>
+                            <p class="card-text">Resistencia 240 Ω, 1/4 W</p>
                             <div class="row">
                                 <div class="col">
-                                    <p class="btn btn-danger btn-block">$0.00 MXN</p>
+                                    <p class="btn btn-danger btn-block">$3.00 MXN</p>
                                 </div>
                                 <div class="col">
-                                    <a href="#" class="btn btn-success btn-block">Add to cart</a>
+                                    <a href="#" class="btn btn-cart btn-block">Add to cart</a>
                                 </div>
                             </div>
                         </div>
@@ -77,33 +97,16 @@
                 </div>
                 <div class="col-12 col-md-6 col-lg-4">
                     <div class="card">
-                        <img class="card-img-top" src="https://dummyimage.com/600x400/55595c/fff" alt="Card image cap">
+                        <img class="card-img-top" src="https://scontent-qro1-1.xx.fbcdn.net/v/t1.0-9/89728073_113712000242162_5490466112787185664_n.png?_nc_cat=100&_nc_sid=0debeb&_nc_ohc=nPkvd5DWxSsAX9jMBNS&_nc_ht=scontent-qro1-1.xx&oh=3710bbc99962e33590916a279ab69e99&oe=5EDFE226" alt="Card image cap">
                         <div class="card-body">
-                            <h4 class="card-title"><a href="product.html" title="View Product">Product title</a></h4>
-                            <p class="card-text">Description.</p>
+                            <h4 class="card-title"><a href="product.html" title="View Product">Resistencia 10Ω, 1W</a></h4>
+                            <p class="card-text">Resistencia 10Ω, 1W</p>
                             <div class="row">
                                 <div class="col">
-                                    <p class="btn btn-danger btn-block">$0.00 MXN</p>
+                                    <p class="btn btn-danger btn-block">$6.00 MXN</p>
                                 </div>
                                 <div class="col">
-                                    <a href="#" class="btn btn-success btn-block">Add to cart</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-12 col-md-6 col-lg-4">
-                    <div class="card">
-                        <img class="card-img-top" src="https://dummyimage.com/600x400/55595c/fff" alt="Card image cap">
-                        <div class="card-body">
-                            <h4 class="card-title"><a href="product.html" title="View Product">Product title</a></h4>
-                            <p class="card-text">Description.</p>
-                            <div class="row">
-                                <div class="col">
-                                    <p class="btn btn-danger btn-block">$0.00 MXN</p>
-                                </div>
-                                <div class="col">
-                                    <a href="#" class="btn btn-success btn-block">Add to cart</a>
+                                    <a href="#" class="btn btn-cart btn-block">Add to cart</a>
                                 </div>
                             </div>
                         </div>
@@ -146,6 +149,12 @@ export default {
 
 <style lang= "scss"scoped>
 
+
+
+.btn-cart{
+    background: linear-gradient(180deg, #0288D1,  #0097A7);
+    color: white;
+}
 .bloc_left_price {
     color: #c01508;
     text-align: center;
@@ -195,4 +204,16 @@ export default {
 .pagination {
     margin-top: 20px;
 }
+.card-img-top{
+    display: block;
+    user-select:none;
+    width: 313px;
+    height: 134.844px;
+
+   
+    
+
+}
 </style>
+
+
